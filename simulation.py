82c16667513394524406c02f724641927169648d
Xu LIU <xliu@imagine-optic.com>
2021-06-28 15:35:06 +0200

    separate probe_Efield and probe_Ifield,add camADconvertor,display diffraction pattern,not support different obj_pxlsize,save_tiff_pattern failed,need to add cross_section

--- simulation.py
+++ simulation.py
@@ -1,5 +1,4 @@
 from __future__ import division
-from operator import le
 # pynx
 from pynx.wavefront import Wavefront, PropagateNearField, PropagateFarField
 
@@ -53,20 +52,19 @@ obj_nearfield = False # True/Flase
 # cam info
 cam_obj_distance = 10e-2 # meter
 cam_pxlsize = 13e-6 # meter
-
 '''
 the camera shape will influence the shape of gaussian,
 recommend to define an square shape camera, which will be less
 problematic.
 '''
 cam_pxlnb = (512,512)
-cam_binning = None
-cam_qe = None
-cam_dark_noise = None # e-
-cam_dark_current = None # e-/pixe/s
-cam_sensitivity = None
+cam_binning = 1
+cam_qe = 0.02
+cam_dark_noise = 2.9 # e-
+cam_dark_current = 3e-4 # e-/pixe/s
+cam_sensitivity = 1
 cam_bitdepth = 16
-cam_baseline = None
+cam_baseline = 100
 
 # probe info
 probe_type_list = ['guass'] # do not change
@@ -80,7 +78,7 @@ in case of an asymetric gaussian.
 '''
 probe_sigma_ratio = 1 
 probe_max_photonnb =  1e5
-probe_bg_photonnb = None
+probe_bg_photonnb = 1e2
 
 # scan info
 scan_type_list = ['rect','spiral'] # do not change
@@ -126,7 +124,7 @@ def verify_array_memory(arrayshape,info='unknown'):
         np.ones(arrayshape)
     except MemoryError:
         print(f'{info} with shape {arrayshape.shape} is too big! simulation stopped!')
-        quit()
+        exit()
 
 '''
 # instead of simulating a probe with giving sigma value which leads to some overextend problems,
@@ -212,10 +210,9 @@ def pad_obj(obj,cam_pxlnb):
     obj_pad[left:left+obj_xpxlnb,bottom:bottom+obj_ypxlnb] = obj[::]
     return obj_pad,obj_pxllim
 
-def make_probe_gauss(probe_shape_pxlnb,probe_sigma_ratio,probe_max_photonnb,center=(0,0)):
+def make_probe_gauss(probe_shape_pxlnb,probe_sigma_ratio,center=(0,0)):
     """
-    return a circularly masked 2D gaussian intensity distribution without rotation, centered at (0,0)
-    sigma is the sigma of an electric field in the form of: exp(-x2/sig2)
+    return a circularly masked 2D gaussian electric field distribution without rotation, centered at (0,0)
     """
     probe_sigma_pxlnb = probe_shape_pxlnb[0]//(2*probe_sigma_ratio),probe_shape_pxlnb[1]//(2*probe_sigma_ratio)
     nx, ny = probe_shape_pxlnb
@@ -225,8 +222,15 @@ def make_probe_gauss(probe_shape_pxlnb,probe_sigma_ratio,probe_max_photonnb,cent
     y = np.linspace(-ny//2, ny//2, ny)
     xx, yy = np.meshgrid(x, y)
 
-    g = np.exp(-2*((xx-center[0])**2/(v[0])+(yy-center[1])**2/(v[1])))
-    return g/g.sum()*probe_max_photonnb
+    g = np.exp(-((xx-center[0])**2/(v[0])+(yy-center[1])**2/(v[1])))
+    return g
+
+def get_probe_gauss_intensity(probe_efield,probe_max_photonnb=1,probe_bg_photonnb=0):
+    """
+    return the intensity distribution of a know electric field.
+    """
+    probe_ifield = probe_efield**2
+    return (probe_ifield/probe_ifield.max())*probe_max_photonnb+probe_bg_photonnb
 
 def rect(pas,nb):
     '''
@@ -294,6 +298,11 @@ def make_scan(scan_type,scan_step_pxlnb,scan_nb):
 
 def align_scan_obj(scan_position,obj_pxlnb,obj_pxlnb_pad,obj_pxllim):
     '''
+    the pynx can reconstruct the obj even if the scanning positions were not given in a sequence,
+    but here I need the scanning position to be a continuous sequece so that I can filter out these
+    positions where the probe exceeds the obj_pxlnb_pad. Hence the function that generate scan position
+    should return the positions in a manner of continious. Otherwise the obj should be scaled to fit
+    the scanning position. In this case, there is no necessary to specify the real size of the obj.
     return: a tuple of aligned scan position
     '''
     objxmid = obj_pxlnb_pad[0]//2
@@ -320,9 +329,9 @@ def align_scan_obj(scan_position,obj_pxlnb,obj_pxlnb_pad,obj_pxllim):
             scanxpos_temp.append(scanxpos[idx])
             scanypos_temp.append(scanypos[idx])
         else:
-            continue
             print(f'probe exceed obj range at scan num.{idx}, corresponding pixel possion is({scanxpos[idx]},{scanypos[idx]}).')
             print('scan point deleted.')
+            break
 
     if len(scanxpos_temp)<=1:
         print(f'no scan falls into the obj, generate one scan at obj center:{objxmid},{objymid}')
@@ -366,6 +375,123 @@ def save_fromarray(array,path_dir_experiment,imagetitle):
     pattern = Image.fromarray(pattern)
     pattern.save(path)
 
+class cameraADconvertor():
+    '''
+    light: the incoming light source.
+    qe: quantum efficiency at corresponding wavelength.
+    dark_noise: depends on read_out speed, for andor the dark_noise
+                is a combination of AD conversion noise and sensor read
+                out noise, measured with single pixel at -80 degree 
+                with minimum exposure time. e-
+    dark_current: overall average of the sensor. e-/pixel/sec
+    binning: bin factor(identical for both side)
+    sensitivity: not psecified in andor.
+    bitdepth: 16bits
+    baseline: not speified in andor.
+    '''
+    def __init__(self,
+                    qe=0.02,
+                    dark_noise=2.9,
+                    dark_current=3e-4,
+                    binning = 1,
+                    sensitivity = 1,
+                    bitdepth = 16,
+                    baseline = 100
+                    ):
+        self.qe = qe
+        self.dark_noise = dark_noise
+        self.dark_current = dark_current
+        self.binning = binning
+        self.sensitivity = sensitivity
+        self.bitdepth = bitdepth
+        self.baseline = baseline
+
+    def applycamera(self,light):
+        self.light = light
+        self.generate_poisson()
+        self.photonToelectron()
+        self.generate_dark_noise()
+        self.applybinning()
+        self.anologTodigital()
+        return(self.light)
+
+    def generate_poisson(self):
+        try:
+            self.light = np.random.poisson(self.light)
+        except ValueError:
+            print('Memory error during the generation of photon noise! Simulation stoped!')
+            exit()
+
+    def photonToelectron(self):
+        self.light = np.round(self.qe*self.light)
+
+    def generate_dark_noise(self):
+        '''
+        the dark noise will be modeled as a gaussian distribution whose standard deviation is
+        equivalent to the dark noise of the camera.
+        '''
+        self.light = np.round(np.random.normal(
+            scale=self.dark_noise,
+            size=self.light.shape)+self.light+self.dark_current)
+    
+    def applybinning(self):
+        if self.binning>1:
+            newshape = self.light.shape[0]//self.binning, self.binning, self.light.shape[1]//self.binning, self.binning
+            self.light.reshape(newshape).sum(-1).sum(1)
+
+    def anologTodigital(self):
+        '''
+        analog-to-digital units
+        '''
+        max_depth = np.int(2**self.bitdepth-1)
+        self.light = (self.light*self.sensitivity).astype(int)
+        self.light += self.baseline
+        self.light[self.light>max_depth] = max_depth
+
+def make_diffraction(obj_pad,probe_Efield,scan_position_align):
+    obj_xpxlnb,obj_ypxlnb = obj_pad.shape
+    probe_shape_xpxlnb,probe_shape_ypxlnb = probe_Efield.shape
+    scan_xposition, scan_yposition = scan_position_align
+    x0 = scan_xposition-probe_shape_xpxlnb//2
+    y0 = scan_yposition-probe_shape_ypxlnb//2
+    if x0<0 or y0<0 or x0+probe_shape_xpxlnb>obj_xpxlnb or y0+probe_shape_ypxlnb>obj_ypxlnb:
+        print('OVER RANGE at scan position(pixels):',scan_xposition,scan_yposition)
+    return probe_Efield*obj_pad[x0:x0+probe_shape_xpxlnb,y0:y0+probe_shape_ypxlnb]
+
+def get_diffration(obj_pad,
+                    probe_Efield,
+                    scan_position_align,
+                    cam,
+                    probe_wavelength,
+                    obj_pxlsize,
+                    obj_nearfield,
+                    cam_obj_distance,
+                    probe_max_photonnb,
+                    probe_bg_photonnb
+                    ):
+    E_diffracted = make_diffraction(obj_pad,probe_Efield,scan_position_align)
+    E_wavefront = Wavefront(d=np.fft.fftshift(E_diffracted), 
+                            wavelength=probe_wavelength,
+                            pixel_size=obj_pxlsize)
+    if obj_nearfield:
+        E_wavefront = PropagateNearField(dz=cam_obj_distance) * E_wavefront
+    else:
+        E_wavefront = PropagateFarField(dz=cam_obj_distance) * E_wavefront
+    # taking photon nb and background
+    I_wavefront = abs(E_wavefront.get(shift=True))**2*probe_max_photonnb+probe_bg_photonnb
+    # problem of dimension
+    I_wavefront = np.squeeze(I_wavefront,axis=0)
+    I_wavefront = cam.applycamera(I_wavefront)
+    return I_wavefront
+
+def save_patterns(path,intensity_temp,name):
+    '''
+    
+    '''
+    file = path+'\\'+str(name)+'.tiff'
+    #intensity_temp= (65535.0/intensity_temp.max()*(intensity_temp-intensity_temp.min())).astype(np.uint16)
+    imagei = Image.fromarray(intensity_temp)
+    imagei.save(file)
 ##########################################################################################
 # premairy calculations and verification
 ##########################################################################################
@@ -412,7 +538,9 @@ print(f'probe_sigma_size is:{probe_sigma_size[0]:.3e},{probe_sigma_size[1]:.3e} 
 '''
 make probe
 '''
-probe = make_probe_gauss(probe_shape_pxlnb,probe_sigma_ratio,probe_max_photonnb)
+probe_Efield = make_probe_gauss(probe_shape_pxlnb,probe_sigma_ratio)
+probe_Ifield = get_probe_gauss_intensity(probe_Efield,probe_max_photonnb,probe_bg_photonnb)
+probe_bg = np.ones((probe_Ifield.shape))*probe_bg_photonnb
 
 # scan
 scan_step_pxlnb = np.array(probe_sigma_pxlnb).min()*scan_sigma_ratio
@@ -425,8 +553,7 @@ scan_position_align = align_scan_obj(scan_position,obj_pxlnb,obj_pxlnb_pad,obj_p
 path_dir_newsimulation,path_dir_experiment,path_dir_diffraction = make_dir(path_dir_working)
 
 # show obj
-fig1 = plt.figure()
-fig1.suptitle('obj image')
+fig1 = plt.figure(tight_layout=True)
 
 ax11 = fig1.add_subplot(121)
 ax11.set_title('amplitude image')
@@ -455,15 +582,14 @@ fig1.savefig(path_obj_image)
 plt.show()
 
 # show probe
-fig2 = plt.figure()
-fig2.suptitle('probe image')
+fig2 = plt.figure(tight_layout=True)
 
 ax21 = fig2.add_subplot(121)
 ax21.set_title('probe linear')
 ax21.set_xlabel('meter')
 ax21.set_ylabel('meter')
 ax21.ticklabel_format(axis='both',style='sci',scilimits=(-6,-6))
-ax21.imshow(probe,
+ax21.imshow(probe_Ifield,
             extent=[0,probe_shape_pxlnb[0]*obj_pxlsize[0],0,probe_shape_pxlnb[1]*obj_pxlsize[1]],
             cmap='Greys_r',
             interpolation='nearest')
@@ -473,7 +599,7 @@ ax22.set_title('probe log10')
 ax22.set_xlabel('meter')
 ax22.set_ylabel('meter')
 ax22.ticklabel_format(axis='both',style='sci',scilimits=(-6,-6))
-ax22.imshow(np.log10(probe),
+ax22.imshow(np.log10(probe_Ifield),
             extent=[0,probe_shape_pxlnb[0]*obj_pxlsize[0],0,probe_shape_pxlnb[1]*obj_pxlsize[1]],
             cmap='Greys_r',
             interpolation='nearest')
@@ -483,52 +609,105 @@ fig2.savefig(path_probe_image)
 plt.show()
 
 # show scan
-fig3 = plt.figure()
-fig3.suptitle('scan')
+fig3 = plt.figure(tight_layout=True)
+grid = fig3.add_gridspec(2,3)
 
-ax31 = fig3.add_subplot(121)
+# init obj as scan position background
+ax31 = fig3.add_subplot(grid[0,0])
 ax31.set_title('scan position')
 ax31.set_xlabel('meter')
 ax31.set_ylabel('meter')
 ax31.ticklabel_format(axis='both',style='sci',scilimits=(-6,-6))
+
 ax31.imshow(np.abs(obj_pad),
             extent=[0,obj_pxlnb_pad[0]*obj_pxlsize[0],0,obj_pxlnb_pad[1]*obj_pxlsize[1]],
             cmap='Greys_r',
             interpolation='nearest')
 
-ax32 = fig3.add_subplot(122)
+# init obj as scan area background
+ax32 = fig3.add_subplot(grid[1,0])
 ax32.set_title('scan area')
 ax32.set_xlabel('meter')
 ax32.set_ylabel('meter')
 ax32.ticklabel_format(axis='both',style='sci',scilimits=(-6,-6))
+
 ax32.imshow(np.abs(obj_pad),
             extent=[0,obj_pxlnb_pad[0]*obj_pxlsize[0],0,obj_pxlnb_pad[1]*obj_pxlsize[1]],
             cmap='Greys_r',
             interpolation='nearest')
+
+# init camera background as diffraction pattern background
+ax33 = fig3.add_subplot(grid[:,1:])
+ax33.set_title('background')
+ax33.set_xlabel('meter')
+ax33.set_ylabel('meter')
+ax33.ticklabel_format(axis='both',style='sci',scilimits=(-6,-6))
+
+dict_camADconverotr ={
+    'qe':cam_qe,
+    'dark_noise':cam_dark_noise,
+    'dark_current':cam_dark_current,
+    'binning':cam_binning,
+    'sensitivity':cam_sensitivity,
+    'bitdepth':cam_bitdepth,
+    'baseline':cam_baseline,
+}
+cam = cameraADconvertor(**dict_camADconverotr)
+cam_bg = cam.applycamera(light=probe_bg)
+save_patterns(path_dir_experiment,cam_bg,'cam_bg')
+
+image33 = ax33.imshow(cam_bg,
+                    extent=[0,obj_pxlnb_pad[0]*obj_pxlsize[0],0,obj_pxlnb_pad[1]*obj_pxlsize[1]],
+                    cmap='Greys_r',
+                    interpolation='nearest')
 plt.pause(2)
 
+# scan nb
 new_sacn_nb = len(scan_position_align[0])
+# scan position in meter with respect to obj
 scan_xposition_real = np.array(scan_position_align[0])*obj_pxlsize[0]
 scan_yposition_real = np.array(scan_position_align[1])*obj_pxlsize[1]
+# displaying colors
 colors = cm.rainbow(np.linspace(0, 1, new_sacn_nb))
+# contour of the illumination area
 circ_radius = np.array(probe_sigma_size).min()*probe_sigma_ratio
 
 for idx in range(new_sacn_nb):
+    # animation of scan position
     xi = scan_xposition_real[idx]
     yi = scan_yposition_real[idx]
     ci = colors[idx]
     ax31.scatter(xi,yi,color=ci)
 
-    circi = mpatches.Circle((xi,yi),radius=circ_radius,color=ci,fill=False)
-    ax32.add_patch(circi)
+    # animation of scan area
+    circ_i = mpatches.Circle((xi,yi),radius=circ_radius,color=ci,fill=False)
+    ax32.add_patch(circ_i)
+
+    # animation of diffraction pattern
+    scan_iposition_align = scan_position_align[0][idx],scan_position_align[1][idx]
+    intensity_temp = get_diffration(obj_pad,
+                                probe_Efield,
+                                scan_iposition_align,
+                                cam,
+                                probe_wavelength,
+                                obj_pxlsize[0], # not support different size
+                                obj_nearfield,
+                                cam_obj_distance,
+                                probe_max_photonnb,
+                                probe_bg_photonnb
+                                )
+    ax33.set_title('scan'+str(idx))
+    image33.set_data(intensity_temp)
+    # saving diffration pattern
+    save_patterns(path_dir_diffraction,intensity_temp,idx)
     plt.pause(1)
 
 path_scan_image = path_dir_experiment + '\\scan_image.tiff'
 fig3.savefig(path_scan_image)
 
-plt.show()
-# 
+plt.pause(2)
 quit()
+
 dict_extrainfo={
     'extra_path_workingdir':extra_path_workingdir,
     'extra_path_simulationdir':extra_path_simulationdir,
