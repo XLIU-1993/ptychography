a7babd71d0c8a3849159c9b8c41494dc41def8dc
Xu LIU <xliu@imagine-optic.com>
2021-07-08 17:46:00 +0200

    Update reconstruction.py
    sort the filelist when import images. reconstruction from objective based simulation is good, need to be optimized.

--- reconstruction.py
+++ reconstruction.py
@@ -2,7 +2,6 @@ from __future__ import division
 
 # ptycho
 from pynx2019.ptycho import*
-from pynx2019.ptycho import shape
 
 # read save files
 import os,sys,json
@@ -20,12 +19,12 @@ def make_probe_gauss(probe_shape_pxlnb,probe_sigma_ratio,center=(0,0)):
     return a circularly masked 2D gaussian electric field distribution without rotation, centered at (0,0)
     identical with the gauss function in simulation of didffraction pattern.
     """
-    probe_sigma_pxlnb = probe_shape_pxlnb[0]//(2*probe_sigma_ratio[0]),probe_shape_pxlnb[1]//(2*probe_sigma_ratio[1])
-    nx, ny = probe_shape_pxlnb
+    probe_sigma_pxlnb = probe_shape_pxlnb[1]//(2*probe_sigma_ratio[0]),probe_shape_pxlnb[0]//(2*probe_sigma_ratio[1])
+    row, column = probe_shape_pxlnb
     v = np.array(probe_sigma_pxlnb) ** 2
 
-    x = np.linspace(-nx//2, nx//2, nx)
-    y = np.linspace(-ny//2, ny//2, ny)
+    x = np.linspace(-column//2, column//2, column)
+    y = np.linspace(-row//2, row//2, row)
     xx, yy = np.meshgrid(x, y)
 
     g = np.exp(-((xx-center[0])**2/(v[0])+(yy-center[1])**2/(v[1])))
@@ -40,49 +39,72 @@ def make_random_obj(obj_pxlnb):
     obj = np.random.uniform(0,1,obj_pxlnb) * np.exp(1j * rand_phase)    
     return obj
 
-def shift_scan(scan_position):
-    '''
-    return scan position centered at (0,0)
-    '''
-    posx = np.array(scan_position[0]).astype(np.float)
-    posy = np.array(scan_position[1]).astype(np.float)
-    meanx = posx.mean()
-    meany = posy.mean()
-    shiftx = posx - meanx
-    shifty = posy - meany
-    return (shiftx,shifty)
+def center_scan(posx,posy):
+    posx = np.array(posx)
+    posy = np.array(posy)
+    posx = posx - posx.mean()
+    posy = posy - posy.mean()
+    return (posx,posy)
 
 def get_scan(path_scan_position):
     '''
-    return scan_position centered at (0,0).
+    return scan_position.
     '''
     posx = []
     posy = []
     with open(path_scan_position,'r') as f:
         scan_reader = csv.reader(f)
         for row in scan_reader:
-            posx.append(row[0])
-            posy.append(row[1])
-    scan_position = shift_scan((posx,posy))
+            posx.append(float(row[0]))
+            posy.append(float(row[1]))
+    scan_position = center_scan(posx,posy)
     return scan_position
+
+def get_background(path_cam_bg):
+    cam_bg = np.array(Image.open(path_cam_bg))
+    return cam_bg
 
 def get_diffraction_patterns(path_dir_diffraction):
     '''
-    return 3D array of diffraction patterns.
+    return 3D array of diffraction patterns in the order of scan.
     '''
     images = []
     valid_images = [".tif",".tiff",".bmp"]
-    for f in os.listdir(path_dir_diffraction):
+    f_list = os.listdir(path_dir_diffraction)
+    f_list.sort(key=lambda x:int(x[:-5]))
+    for f in f_list:
         ext = os.path.splitext(f)[1]
         if ext.lower() not in valid_images:
             continue
         images.append(np.array(Image.open(os.path.join(path_dir_diffraction,f))))
     return images
+
+def calc_obj_pxlsize(probe_wavelength,cam_obj_distance,cam_pxlnb,cam_pxlsize,nearfield):
+    '''
+    return a tuple of obj_pxlsize(xpxlsize,ypixelsize)
+    '''
+    if nearfield:
+        obj_xpxlsize = cam_pxlsize
+        obj_ypxlsize = cam_pxlsize
+    else:
+        obj_xpxlsize = probe_wavelength*cam_obj_distance/(cam_pxlnb[1]*cam_pxlsize)
+        obj_ypxlsize = probe_wavelength*cam_obj_distance/(cam_pxlnb[0]*cam_pxlsize)
+    obj_pxlsize = (obj_xpxlsize,obj_ypxlsize)
+    return obj_pxlsize
+
+def calc_obj_pxlnb(posx, posy, cam_pxlnb):
+    """
+    return the required pxl nb for the reconstructed object.
+    """
+    ny = int(2 * (abs(np.ceil(posy)) + 4).max() + cam_pxlnb[0])
+    nx = int(2 * (abs(np.ceil(posx)) + 4).max() + cam_pxlnb[1])
+    return ny, nx
+
 ##########################################################################################
 # import simulationd data
 ##########################################################################################
 # give simulation directory
-path_dir_simulation = 'D:\\scripts\\ptychography\\202107051320_ptycho_simulation'
+path_dir_simulation = r'D:\scripts\ptychography\202107081707_ptycho_simulation' # DEFINE
 # auto fill directory
 path_dir_experiment = path_dir_simulation+'\\simulation_info'
 path_dir_diffraction = path_dir_simulation+'\\diffraction_patterns'
@@ -97,10 +119,15 @@ with open(path_simulation_info,'r') as f:
 
 # read scan position info
 scan_position = get_scan(path_scan_position)
-print('scqn nb',len(scan_position[0]))
+print('scan nb',len(scan_position[0]))
+
+# read cam_bg
+cam_bg = get_background(path_cam_bg)
 
 # read diffraction pattern
-intensity = get_diffration
+intensity = get_diffraction_patterns(path_dir_diffraction)
+
+
 ##########################################################################################
 # set parameters for known cam_obj_distance
 ##########################################################################################
@@ -109,32 +136,71 @@ cam_pxlnb = dict_simulationinfo['cam_info']['cam_pxlnb']
 cam_pxlsize = dict_simulationinfo['cam_info']['cam_pxlsize']
 probe_wavelength = dict_simulationinfo['probe_info']['probe_wavelength']
 cam_obj_distance = dict_simulationinfo['cam_info']['cam_obj_distance']
+obj_nearfield = dict_simulationinfo['obj_info']['obj_nearfield']
 
 print(f'cam_pxlnb:{cam_pxlnb} .')
-print(f'cam_pxlsize:{cam_pxlsize} meter.')
-print(f'probe_wavelength:{probe_wavelength} meter.')
-print(f'cam_obj_distance:{cam_obj_distance} meter.')
-
-# probe
-'''
-out of the radius of probe_sigma_ratio*probe_shape_pxlnb, 
-the intensity will be considered as zero, recommend to set the ratio as 
-(2,2), since gauss intensity distribution outside 2sigma is consider as 0.
-'''
-probe_shape_pxlnb = cam_pxlnb
-probe_sigma_ratio = (2,2)  # DEFINE
-
-# obj
- 
-
+print(f'cam_pxlsize:{cam_pxlsize:.3e} meter.')
+print(f'probe_wavelength:{probe_wavelength:.3e} meter.')
+print(f'cam_obj_distance:{cam_obj_distance:.3e} meter.')
+print(f'obj_nearfield:{obj_nearfield}.')
 
 ##########################################################################################
 # initial guess
 ##########################################################################################
-# probe
-probe_Efield = make_probe_gauss(probe_shape_pxlnb,probe_sigma_ratio)
-
 # obj
-obj_pxlsize = dict_simulationinfo['obj_info_sup']['obj_pxlsize']
-print(f'obj_pxlsize:{obj_pxlsize} meter.')
-#obj_pxlnb = shape.calc_obj_shape(posx/pixel_size_object, posy/pixel_size_object, ampl.shape[1:])
+obj_pxlsize = calc_obj_pxlsize(probe_wavelength,
+                                cam_obj_distance,
+                                cam_pxlnb,
+                                cam_pxlsize,
+                                obj_nearfield)
+
+obj_pxlnb_pad = calc_obj_pxlnb(scan_position[0]/obj_pxlsize[0],
+                                scan_position[1]/obj_pxlsize[1],
+                                cam_pxlnb)
+
+obj_init = make_random_obj(obj_pxlnb_pad)
+
+# probe
+probe_shape_pxlnb = cam_pxlnb
+probe_sigma_ratio = (3,3)  # DEFINE
+
+probe_shape_size = probe_shape_pxlnb[1]*obj_pxlsize[0],probe_shape_pxlnb[0]*obj_pxlsize[1]  # width height
+probe_sigma_pxlnb = probe_shape_pxlnb[1]//(2*probe_sigma_ratio[0]),probe_shape_pxlnb[0]//(2*probe_sigma_ratio[1]) # width height
+probe_sigma_size = probe_sigma_pxlnb[0]*obj_pxlsize[0],probe_sigma_pxlnb[1]*obj_pxlsize[1]  # width height
+print(f'probe_shape_pxlnb:{probe_shape_pxlnb}.')
+print(f'probe_shape_size is:{probe_shape_size[0]:.3e},{probe_shape_size[1]:.3e} meter.')
+print(f'probe_sigma_pxlnb:{probe_sigma_pxlnb}.')
+print(f'probe_sigma_size is:{probe_sigma_size[0]:.3e},{probe_sigma_size[1]:.3e} meter.')
+
+probe_Efield_init = make_probe_gauss(probe_shape_pxlnb,probe_sigma_ratio)
+
+##########################################################################################
+# reconstruction
+##########################################################################################
+iteration_nb = 100 #DEFINE
+do_ml = False #DEFINE
+do_dm = True #DEFINE
+do_ap = False #DEFINE
+
+p_data = PtychoData(iobs=intensity, 
+                    positions=(np.array(scan_position[0]),np.array(scan_position[1])), 
+                    detector_distance=cam_obj_distance, 
+                    pixel_size_detector=cam_pxlsize, 
+                    wavelength=probe_wavelength)
+
+p = Ptycho(probe=probe_Efield_init,
+            obj=obj_init, 
+            data=p_data, 
+            background=cam_bg)
+
+p = ScaleObjProbe(verbose=True) * p
+if do_ml:
+    p = ML(update_object=True, update_probe=True, calc_llk=5, show_obj_probe=5) ** iteration_nb * p
+if do_dm:
+    p = DM(update_object=True, update_probe=True, calc_llk=5, show_obj_probe=5) ** iteration_nb * p
+if do_ap:
+    p = AP(update_object=True, update_probe=True, calc_llk=5, show_obj_probe=5) ** iteration_nb * p
+
+
+
+
